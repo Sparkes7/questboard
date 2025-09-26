5c93e23a7d1fcbfb488e742f85bbb0551317eb34
Sparkes7 <willsparkes93@gmail.com>
2025-09-26 19:39:11 +0100

    .

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { db } from "./dbConnection.js";
 import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 app.use(express.json());
